730a1c1312d6ab2afdea5d81eebd640ec39dd11d
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-02-14 18:51:53 +0530

    Added line variables.

--- calc-ctrl.js
+++ calc-ctrl.js
@@ -5,15 +5,17 @@ function CalcCtrl($scope) {
     $scope.code = [
         'a = 2',
         'a',
-        'sin(pi/4) * sqrt(a) + 42'
+        'sin(pi/4) * sqrt(a) + 42',
+        'L3'
     ].join('\n');
 
     $scope.results = function () {
         var lines = $scope.code.split('\n'),
             evaluator = new TapDigit.Evaluator(),
             results = [];
-        for (var i = 0; i < lines.length; i++) {
-            results.push(evaluator.evaluate(lines[i]));
+        for (var i = 0, len = lines.length; i < len; i++) {
+            var code = 'L' + (i + 1) + ' = ' + lines[i];
+            results.push(evaluator.evaluate(code));
         }
         return results;
     };
