bbc89fc784def370b6c87ba8c257bccc0ad0a275
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-04-06 21:57:20 +0530

    Hotkey for wrapping in parens.

--- app.js
+++ app.js
@@ -35,8 +35,45 @@
 
     codeInput.addEventListener('change', updateSheet);
     codeInput.addEventListener('click', updateSheet);
+
     codeInput.addEventListener('keydown', function (e) {
+
+        // <C-b> - Wrap in parens.
+        if (e.ctrlKey && e.which === 66) {
+
+            var oldCode = codeInput.value,
+                start = codeInput.selectionStart,
+                end = codeInput.selectionEnd,
+                prefix = oldCode.substr(0, start),
+                selection = oldCode.substr(start, end - start),
+                suffix = oldCode.substr(end);
+
+            if (selection) {
+                // Wrap the selection in parens.
+                codeInput.value =
+                    [prefix, '(', selection, ')', suffix].join('');
+                codeInput.selectionStart = start + 1;
+                codeInput.selectionEnd = end + 1;
+
+            } else {
+                // Nothing selection. Wrap whole current line in parens.
+                var nlBegin = prefix.split('').reverse().indexOf('\n'),
+                    nlEnd = suffix.indexOf('\n'),
+                    lineStart = nlBegin === -1 ? 0 : prefix.length - nlBegin,
+                    lineEnd = nlEnd === -1 ?
+                        oldCode.length : prefix.length + nlEnd;
+
+                codeInput.value = oldCode.substr(0, lineStart) + '(' +
+                    oldCode.substr(lineStart, lineEnd - lineStart) + ')' +
+                    oldCode.substr(lineEnd);
+
+                codeInput.selectionStart = codeInput.selectionEnd = start + 1;
+
+            }
+        }
+
         setTimeout(updateSheet, 0);
+
     });
 
     codeInput.value = [
