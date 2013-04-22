b50f35d8939502f16eba7b2a3dab598ecd25e869
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-04-22 21:28:05 +0530

    Folding pretty functional.

--- lake/ace-mode.js
+++ lake/ace-mode.js
@@ -57,71 +57,29 @@ ace.define('lake/ace-mode', function(require, exports, module) {
     exports.Mode = Mode;
 });
 
-ace.define('lake/ace-mode-js', function(require, exports, module) {
 
+ace.define('lake/ace-mode-js', function(require, exports, module) {
 
 var oop = require('ace/lib/oop'),
     TextMode = require('ace/mode/text').Mode,
     Tokenizer = require('ace/tokenizer').Tokenizer,
     LakeHighlightRules = require('lake/highlight-rules').LakeHighlightRules,
-    MatchingBraceOutdent = require('ace/mode/matching_brace_outdent').MatchingBraceOutdent,
     Range = require('ace/range').Range,
     WorkerClient = require('ace/worker/worker_client').WorkerClient,
     CstyleBehaviour = require('ace/mode/behaviour/cstyle').CstyleBehaviour,
-    CStyleFoldMode = require('ace/mode/folding/cstyle').FoldMode;
+    FoldMode = require('lake/fold-mode').FoldMode;
 
 var Mode = function() {
     this.$tokenizer = new Tokenizer(new LakeHighlightRules().getRules());
-    this.$outdent = new MatchingBraceOutdent();
     this.$behaviour = new CstyleBehaviour();
-    this.foldingRules = new CStyleFoldMode();
+    this.foldingRules = new FoldMode();
 };
 oop.inherits(Mode, TextMode);
 
 (function() {
 
-    this.lineCommentStart = "//";
-    this.blockComment = {start: "/*", end: "*/"};
-
-    this.getNextLineIndent = function(state, line, tab) {
-        var indent = this.$getIndent(line);
-
-        var tokenizedLine = this.$tokenizer.getLineTokens(line, state);
-        var tokens = tokenizedLine.tokens;
-        var endState = tokenizedLine.state;
-
-        if (tokens.length && tokens[tokens.length-1].type == "comment") {
-            return indent;
-        }
-
-        if (state == "start" || state == "no_regex") {
-            var match = line.match(/^.*(?:\bcase\b.*\:|[\{\(\[])\s*$/);
-            if (match) {
-                indent += tab;
-            }
-        } else if (state == "doc-start") {
-            if (endState == "start" || endState == "no_regex") {
-                return "";
-            }
-            var match = line.match(/^\s*(\/?)\*/);
-            if (match) {
-                if (match[1]) {
-                    indent += " ";
-                }
-                indent += "* ";
-            }
-        }
-
-        return indent;
-    };
-
-    this.checkOutdent = function(state, line, input) {
-        return this.$outdent.checkOutdent(line, input);
-    };
-
-    this.autoOutdent = function(state, doc, row) {
-        this.$outdent.autoOutdent(doc, row);
-    };
+    // TODO: What the hell is this for?
+    this.lineCommentStart = ';';
 
     // TODO: Use the lake parser here.
     // this.createWorker = function(session) {
@@ -144,8 +102,8 @@ oop.inherits(Mode, TextMode);
 exports.Mode = Mode;
 });
 
-ace.define('lake/highlight-rules', function(require, exports, module) {
 
+ace.define('lake/highlight-rules', function(require, exports, module) {
 
 var oop = require('ace/lib/oop');
 var TextHighlightRules = require('ace/mode/text_highlight_rules').TextHighlightRules;
@@ -168,48 +126,8 @@ oop.inherits(LakeHighlightRules, TextHighlightRules);
 exports.LakeHighlightRules = LakeHighlightRules;
 });
 
-ace.define('ace/mode/matching_brace_outdent', ['require', 'exports', 'module' , 'ace/range'], function(require, exports, module) {
-
-
-var Range = require("../range").Range;
-
-var MatchingBraceOutdent = function() {};
-
-(function() {
-
-    this.checkOutdent = function(line, input) {
-        if (! /^\s+$/.test(line))
-            return false;
-
-        return /^\s*\}/.test(input);
-    };
-
-    this.autoOutdent = function(doc, row) {
-        var line = doc.getLine(row);
-        var match = line.match(/^(\s*\})/);
-
-        if (!match) return 0;
-
-        var column = match[1].length;
-        var openBracePos = doc.findMatchingBracket({row: row, column: column});
-
-        if (!openBracePos || openBracePos.row == row) return 0;
-
-        var indent = this.$getIndent(doc.getLine(openBracePos.row));
-        doc.replace(new Range(row, 0, row, column-1), indent);
-    };
-
-    this.$getIndent = function(line) {
-        return line.match(/^\s*/)[0];
-    };
-
-}).call(MatchingBraceOutdent.prototype);
-
-exports.MatchingBraceOutdent = MatchingBraceOutdent;
-});
 
 ace.define('ace/mode/behaviour/cstyle', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/mode/behaviour', 'ace/token_iterator', 'ace/lib/lang'], function(require, exports, module) {
-
 
 var oop = require("../../lib/oop");
 var Behaviour = require("../behaviour").Behaviour;
@@ -221,7 +139,6 @@ var SAFE_INSERT_IN_TOKENS =
 var SAFE_INSERT_BEFORE_TOKENS =
     ["text", "paren.rparen", "punctuation.operator", "comment"];
 
-
 var autoInsertedBrackets = 0;
 var autoInsertedRow = -1;
 var autoInsertedLineEnd = "";
@@ -231,7 +148,7 @@ var maybeInsertedLineStart = "";
 var maybeInsertedLineEnd = "";
 
 var CstyleBehaviour = function () {
-    
+
     CstyleBehaviour.isSaneInsertion = function(editor, session) {
         var cursor = editor.getCursorPosition();
         var iterator = new TokenIterator(session, cursor.row, cursor.column);
@@ -244,11 +161,11 @@ var CstyleBehaviour = function () {
         return iterator.getCurrentTokenRow() !== cursor.row ||
             this.$matchTokenType(iterator.getCurrentToken() || "text", SAFE_INSERT_BEFORE_TOKENS);
     };
-    
+
     CstyleBehaviour.$matchTokenType = function(token, types) {
         return types.indexOf(token.type || token) > -1;
     };
-    
+
     CstyleBehaviour.recordAutoInsert = function(editor, session, bracket) {
         var cursor = editor.getCursorPosition();
         var line = session.doc.getLine(cursor.row);
@@ -258,7 +175,7 @@ var CstyleBehaviour = function () {
         autoInsertedLineEnd = bracket + line.substr(cursor.column);
         autoInsertedBrackets++;
     };
-    
+
     CstyleBehaviour.recordMaybeInsert = function(editor, session, bracket) {
         var cursor = editor.getCursorPosition();
         var line = session.doc.getLine(cursor.row);
@@ -269,26 +186,26 @@ var CstyleBehaviour = function () {
         maybeInsertedLineEnd = line.substr(cursor.column);
         maybeInsertedBrackets++;
     };
-    
+
     CstyleBehaviour.isAutoInsertedClosing = function(cursor, line, bracket) {
         return autoInsertedBrackets > 0 &&
             cursor.row === autoInsertedRow &&
             bracket === autoInsertedLineEnd[0] &&
             line.substr(cursor.column) === autoInsertedLineEnd;
     };
-    
+
     CstyleBehaviour.isMaybeInsertedClosing = function(cursor, line) {
         return maybeInsertedBrackets > 0 &&
             cursor.row === maybeInsertedRow &&
             line.substr(cursor.column) === maybeInsertedLineEnd &&
             line.substr(0, cursor.column) == maybeInsertedLineStart;
     };
-    
+
     CstyleBehaviour.popAutoInsertedClosing = function() {
         autoInsertedLineEnd = autoInsertedLineEnd.substr(1);
         autoInsertedBrackets--;
     };
-    
+
     CstyleBehaviour.clearMaybeInsertedClosing = function() {
         maybeInsertedBrackets = 0;
         maybeInsertedRow = -1;
@@ -531,54 +448,24 @@ oop.inherits(CstyleBehaviour, Behaviour);
 exports.CstyleBehaviour = CstyleBehaviour;
 });
 
-ace.define('ace/mode/folding/cstyle', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/range', 'ace/mode/folding/fold_mode'], function(require, exports, module) {
 
+ace.define('lake/fold-mode', function(require, exports, module) {
 
-var oop = require("../../lib/oop");
-var Range = require("../../range").Range;
-var BaseFoldMode = require("./fold_mode").FoldMode;
+var oop = require('ace/lib/oop');
+var BaseFoldMode = require('ace/mode/folding/fold_mode').FoldMode;
 
-var FoldMode = exports.FoldMode = function(commentRegex) {
-    if (commentRegex) {
-        this.foldingStartMarker = new RegExp(
-            this.foldingStartMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.start)
-        );
-        this.foldingStopMarker = new RegExp(
-            this.foldingStopMarker.source.replace(/\|[^|]*?$/, "|" + commentRegex.end)
-        );
-    }
-};
+var FoldMode = exports.FoldMode = function () { };
 oop.inherits(FoldMode, BaseFoldMode);
 
 (function() {
 
-    this.foldingStartMarker = /(\{|\[)[^\}\]]*$|^\s*(\/\*)/;
-    this.foldingStopMarker = /^[^\[\{]*(\}|\])|^[\s\*]*(\*\/)/;
+    this.foldingStartMarker = /\{$/;
+    this.foldingStopMarker = /\}$/;
 
-    this.getFoldWidgetRange = function(session, foldStyle, row) {
-        var line = session.getLine(row);
-        var match = line.match(this.foldingStartMarker);
-        if (match) {
-            var i = match.index;
-
-            if (match[1])
-                return this.openingBracketBlock(session, match[1], row, i);
-
-            return session.getCommentFoldRange(row, i + match[0].length, 1);
-        }
-
-        if (foldStyle !== "markbeginend")
-            return;
-
-        var match = line.match(this.foldingStopMarker);
-        if (match) {
-            var i = match.index + match[0].length;
-
-            if (match[1])
-                return this.closingBracketBlock(session, match[1], row, i);
-
-            return session.getCommentFoldRange(row, i, -1);
-        }
+    this.getFoldWidgetRange = function (session, foldStyle, row) {
+        var match = session.getLine(row).match(this.foldingStartMarker);
+        return match &&
+            this.openingBracketBlock(session, match[0], row, match.index);
     };
 
 }).call(FoldMode.prototype);
